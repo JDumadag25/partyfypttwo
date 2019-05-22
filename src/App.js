e9085c8faa76093b0560d72a4ca8059f536d267a
JDumadag25 <justin.dumadag@gmail.com>
2019-05-21 22:46:36 -0400

    starting with spotify player

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MusicPlayer from './components/MusicPlayer'
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MusicPlayer/>
     </div>
   );
 }
---
+++ src/components/MusicPlayer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MusicPlayer extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>music player</h2>
+      </div>
+    )
+  }
+}
+
+export default MusicPlayer
