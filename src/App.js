855bc92d5f53d01ab4cd70c1ba6777691cb0d0a1
JDumadag25 <justin.dumadag@gmail.com>
2019-05-22 13:17:11 -0400

    working on token

--- src/App.js
+++ src/App.js
@@ -19,13 +19,59 @@ class App extends React.Component {
       duration: 0,
     };
   }
-  render() {
-    /* ... */
-    return(
-      <MusicPlayer/>
-    )
-  }
 
+  handleLogin() {
+  if (this.state.token !== "") {
+    this.setState({ loggedIn: true });
+  }
+}
+
+  render() {
+  const {
+    token,
+    loggedIn,
+    artistName,
+    trackName,
+    albumName,
+    error,
+    position,
+    duration,
+    playing, } = this.state;
+
+  return (
+    <div className="App">
+         <div className="App-header">
+           <h2>Now Playing</h2>
+           <p>A Spotify Web Playback API Demo.</p>
+         </div>
+
+         {error && <p>Error: {error}</p>}
+
+         {loggedIn ?
+         (<div>
+           <p>Artist: {artistName}</p>
+           <p>Track: {trackName}</p>
+           <p>Album: {albumName}</p>
+         </div>)
+         :
+         (<div>
+           <p className="App-intro">
+             Enter your Spotify access token. Get it from{" "}
+             <a href="https://beta.developer.spotify.com/documentation/web-playback-sdk/quick-start/#authenticating-with-spotify">
+               here
+             </a>.
+           </p>
+           <p>
+             <input type="text" value={token} onChange={e => this.setState({ token: e.target.value })} />
+           </p>
+           <p>
+             <button onClick={() => this.handleLogin()}>Go</button>
+           </p>
+         </div>)
+         }
+       </div>
+  );
+ }
 }
 
 export default App;
