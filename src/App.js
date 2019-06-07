e9e1e6c0dc0902ba75ed4c55b1249ba0edf5a6ec
JDumadag25 <justin.dumadag@gmail.com>
2019-06-06 23:02:28 -0400

    adding topbar for spotify server not working

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MusicPlayer from './components/MusicPlayer'
+import TopBar from './components/TopBar'
 
 class App extends React.Component {
   render(){
     return (
+      <div>
+      <TopBar/>
       <MusicPlayer/>
+      </div>
     )
   }
 }
---
+++ src/components/TopBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+class TopBar extends React.Component{
+  render(){
+    return(
+      <div>
+        <Button>Click Me</Button>
+      </div>
+    )
+  }
+}
+
+export default TopBar
