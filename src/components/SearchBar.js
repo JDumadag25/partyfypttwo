25ecde1d4cb68d242c724e135da67904c8ff1a5a
JDumadag25 <justin.dumadag@gmail.com>
2019-06-26 23:17:00 -0400

    figuring out search with semantic

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,49 @@
 import React from 'react'
-import { Search } from 'semantic-ui-react'
+import { Button, Checkbox, Form, List } from 'semantic-ui-react'
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotifyApi = new SpotifyWebApi
 
 class SearchBar extends React.Component{
+
+  state = {
+  query:'',
+  results:[]
+}
+
+  componentWillMount() {
+    this.resetComponent()
+  }
+
+  resetComponent = () => this.setState({ results: [], query: '' })
+
+  handleSearchChange = (e) => {
+    this.setState({query: e.target.value})
+
+    setTimeout(() => {
+    if (this.state.query.length < 1) return this.resetComponent()
+
+    spotifyApi.search(this.state.query, ['track'], {limit: 10})
+    .then(res => this.setState({results: res.tracks.items}))
+      }, 500)
+   }
+
   render(){
+
+    const songSearch = this.state.results.map(result => {
+      return <li>{result}</li>
+    })
+
     return(
-      <Search></Search>
+    <div>
+      <Form>
+        <Form.Field style={{marginRight:45}}>
+          <label>Add a song to the Playlist</label>
+          <input placeholder='Search for Song Title' onChange={this.handleSearchChange}  />
+        </Form.Field>
+      </Form>
+      <div class="eight wide column" style={{overflow: 'auto', maxHeight: 440, padding: 50}}>{songSearch}</div>
+    </div>
     )
   }
 }
